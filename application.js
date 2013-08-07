bd99cb8b60c8bab4562fe6626e170f33aa255dfb
Mike Murphy <mplamurphy1996@gmail.com>
2013-08-07 19:41:09 -0400

    Updates from comments: cleanup unused code. Use PNG for asterisk.

--- application.js
+++ application.js
@@ -1,38 +1,34 @@
 $( document ).ready(function() {
+    /* On enter of each individual product block:
+     * 1. hide headers
+     * 2. show products header text
+     * 3. add red arrow from block to header */
     $("#pr_bc").on("mouseenter", function() {
        hideHeaders();
-       //showHeaders("bc");
        $(this).closest(".main").find("#hdr_bc").show();
        $(this).find("#hdr_img_bc").show();
     });
     $("#pr_hr").on("mouseenter", function() {
        hideHeaders();
-       //showHeaders("hr");
        $(this).closest(".main").find("#hdr_hr").show();
        $(this).find("#hdr_img_hr").show();
    });
     $("#pr_cf").on("mouseenter", function() {
        hideHeaders();
-       //showHeaders("cf");
        $(this).closest(".main").find("#hdr_cf").show();
        $(this).find("#hdr_img_cf").show();
    });
+    
+    /* when moving mouse out of product blocks, remove all headers, and replace with default */
     $(".product").on("mouseleave", function() {
        hideHeaders();
        $(this).closest(".main").find(".hdr_default").show();
     });
 });
 
+/* Hides all content for the header section of the page. Default message, all hdr types, and the arrows  */
 function hideHeaders() {
-    $(".hdr_default").hide();
+    $(".hdr_default").hide(); 
     $(".hdr").hide();
     $(".hdr_img").hide();
-}
-
-function showHeaders(suffix) {
-    var hdrline = "#hdr_"+suffix;
-    var imgline = "#hdr_img_"+suffix;
-    alert(hdrline + "  <-->  " + imgline);
-    $(this).closest(".main").find(hdrline).show();
-    $(this).find(imgline).show();
 }
